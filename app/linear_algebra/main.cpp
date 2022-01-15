42b5ca0c8e8b78a4afb6569c1ef4a6c2a3c4f2f5
Weber, Daniel <48121307+weberdaniel@users.noreply.github.com>
2022-01-15 23:12:56 +0100

    update

--- app/linear_algebra/main.cpp
+++ app/linear_algebra/main.cpp
@@ -5,9 +5,13 @@ using Eigen::MatrixXd;
 
 typedef Eigen::Matrix<float, 3, 3> MyMatrix33f;
 typedef Eigen::Matrix<float, 3, 1> MyVector3f;
+typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> MyMatrix;
 
-int main() 
-{
+
+int main() {
+  MyMatrix33f a;
+  MyVector3f v;
+  MyMatrix mym(10, 15);
   MatrixXd m(2, 2);
   m(0,0) = 3;
   m(1,0) = 2.5;

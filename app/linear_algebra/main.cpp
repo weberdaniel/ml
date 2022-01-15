714249d52578cffd6c8e5f08904a84f179dc1090
Weber, Daniel <48121307+weberdaniel@users.noreply.github.com>
2022-01-15 23:56:32 +0100

    update

--- app/linear_algebra/main.cpp
+++ app/linear_algebra/main.cpp
@@ -39,4 +39,16 @@ int main() {
   std::vector<float> data2 = {1,2,3,4,5,6,8,9};
   Eigen::Map<MyMatrix33f> n(data2.data());
 
+  // Math
+  // what are the types?
+  auto x = Eigen::Matrix2d::Random();
+  auto y = Eigen::Matrix2d::Random();
+  auto res = x + y;
+  auto res2 = x.array() * y.array(); // element wise multiplication
+  // x = y + x;
+  auto res3 = x.array() / y.array();
+  //x += y;
+  //r = x * y;  // matrix multi
+  auto res4 = x.array() * 3;
+
 }
